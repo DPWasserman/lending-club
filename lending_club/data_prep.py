a94542a2d99b692c5c36eab9da35c7c52f2a30d6
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-17 18:02:32 -0400

    Split data file by year

--- lending_club/data_prep.py
+++ lending_club/data_prep.py
@@ -6,9 +6,20 @@ sys.path.append('../lending_club')
 import config
 
 def get_lending_club_data(data_location: str = config.APPROVED_LOANS_CSV):
-    accepted_loans = dd.read_csv(data_location, 
-                       dtype={'desc': 'object',
-                              'id': 'object',
-                              'sec_app_earliest_cr_line': 'object'}, 
-                       low_memory=False)
-    return accepted_loans
+       accepted_loans = dd.read_csv(data_location,
+                                    dtype={'desc': 'object', 
+                                           'id': 'object',
+                                           'sec_app_earliest_cr_line': 'object'}, 
+                                    parse_dates = ['issue_d',],
+                                    low_memory=False)
+       return accepted_loans
+
+def split_file_by_year(df):
+       df = df.dropna(subset=['issue_d'])
+       df['Year'] = df.issue_d.dt.year
+       for year in df.Year.unique():
+              year_excerpt_df = df.loc[df.Year==year,:]
+              #filename = config.DATAPATH / f'accepted_{year}.csv'
+              #year_excerpt_df.to_csv(filename, index=False)
+              filename = config.DATAPATH / f'accepted-{year}.parquet'
+              year_excerpt_df.to_parquet(filename, engine='fastparquet')
