d4468ee02d63fb9f8de493f1a7db2d541b1d6d24
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-19 18:27:31 -0400

    Bug fixes

--- lending_club/config.py
+++ lending_club/config.py
@@ -10,7 +10,8 @@ APPROVED_LOANS_CSV = DATAPATH / 'accepted_2007_to_2018Q4.csv'
 if not os.path.exists(APPROVED_LOANS_CSV):
     raise OSError(f'{APPROVED_LOANS_CSV} does not exist! Please place in the data directory.')
 
-SELECTED_FEATURES = ['addr_state', # Need to dummify
+SELECTED_FEATURES = ['id',
+                     'addr_state', # Need to dummify
                      'annual_inc',
                      'application_type', # Need to binarize 
                      'disbursement_method', # Need to binarize
