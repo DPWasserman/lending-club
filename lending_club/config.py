c84ef27a4969b751fdf0a9fa395253c3d32a6d73
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-19 18:14:19 -0400

    Collectively chose the features

--- lending_club/config.py
+++ lending_club/config.py
@@ -10,6 +10,31 @@ APPROVED_LOANS_CSV = DATAPATH / 'accepted_2007_to_2018Q4.csv'
 if not os.path.exists(APPROVED_LOANS_CSV):
     raise OSError(f'{APPROVED_LOANS_CSV} does not exist! Please place in the data directory.')
 
-SELECTED_FEATURES = [  
-
+SELECTED_FEATURES = ['addr_state', # Need to dummify
+                     'annual_inc',
+                     'application_type', # Need to binarize 
+                     'disbursement_method', # Need to binarize
+                     'dti',
+                     'earliest_cr_line',
+                     'emp_length', # Need to convert to number and add NAs
+                     'emp_title', # Needs to be encoded
+                     'fico_range_high', 
+                     'fico_range_low',
+                     'grade', # Need to dummify or be ordinal encoded
+                     'home_ownership', # Need to dummify
+                     'initial_list_status', # Need to dummify (binarize)
+                     'installment',
+                     'int_rate',
+                     'issue_d',
+                     'loan_amnt',
+                     'open_acc', 
+                     'pub_rec', 
+                     'pub_rec_bankruptcies',
+                     'purpose', # Need to dummify
+                     'sub_grade', # Need to dummify or be ordinal encoded
+                     'term',
+                     'verification_status',
+                     'zip_code' # Need to dummify
                     ]
+
+TARGET_COL = 'loan_status' # Only look at Fully Paid/Charged Off
--- lending_club/data_prep.py
+++ lending_club/data_prep.py
@@ -1,3 +1,4 @@
+import dask.array as da
 import dask.dataframe as dd
 import numpy as np
 import pandas as pd
@@ -18,30 +19,44 @@ def get_lending_club_data(data_location = config.APPROVED_LOANS_CSV, clean_file:
                                     dtype={'desc': 'object', 
                                            'id': 'object',
                                            'sec_app_earliest_cr_line': 'object'}, 
-                                    parse_dates = ['issue_d',],
+                                    parse_dates = ['issue_d','earliest_cr_line'],
                                     low_memory=False)
        elif extension == 'parquet':
               accepted_loans = dd.read_parquet(data_location,
                                                engine='fastparquet')
        else:
               raise ValueError('Bad extension! Please try another file type.')
+
        if clean_file:
+              accepted_loans = accepted_loans.loc[:, config.SELECTED_FEATURES + config.TARGET_COL]
               accepted_loans = clean(accepted_loans)
+       
        if filename_to_save:
               accepted_loans.to_parquet(config.DATAPATH / filename_to_save, engine='fastparquet')
+       
        return accepted_loans
 
 def clean(df):
        """Performs cleaning on the raw dataset"""
-       df = df.dropna(subset=['issue_d']) # If there is no issue date, this is not a "good" record
+       df = df.dropna(subset=['issue_d', # If there is no issue date, this is not a "good" record
+                              'annual_inc',
+                              'dti',
+                              'open_acc',
+                              'pub_rec',
+                              'pub_rec_bankruptcies',
+                              ]) 
        df['id'] = df['id'].astype(int)
        df = df.set_index('id')
        df = df.loc[df['grade'].isin(['A','B','C','D','E']),:] # Remove grades in F or G
+       df = df.loc[df['loan_status'].isin(['Charged Off','Fully Paid']),:] # Remove loans that have not finalized
+       df = df[df.annual_inc < 2e7] # Remove outliers
        return df
 
 def create_features(df):
        """Creates features on the raw dataset"""
-       df['annual_inc_total'] = df['annual_inc_joint'].fillna(df['annual_inc'])
+       #df['annual_inc_total'] = df['annual_inc_joint'].fillna(df['annual_inc'])
+       
+       #df['dti_total'] = df['dti_joint'].fillna(df['dti'])
        return df
 
 def split_file_by_year(df):
@@ -51,3 +66,14 @@ def split_file_by_year(df):
               year_excerpt_df = df.loc[df.Year==year,:]
               filename = config.DATAPATH / f'accepted-{year}.parquet'
               year_excerpt_df.to_parquet(filename, engine='fastparquet')
+
+
+def make_binary(df):
+       """Convert columns to binary"""
+       X = dfl['application_type'].to_dask_array()
+       y = dfl['disbursement_method'].to_dask_array()
+       df['application_type_indiv']= da.where(x =='Individual',1,0)
+       df['disbursement_method_cash']= da.where(y =='Cash',1,0)
+       df = df.drop(['application_type','disbursement_method'], axis
+       return df
+
