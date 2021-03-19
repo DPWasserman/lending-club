ed10ab58f3d666c7e542f8e5c2ded27b7c193909
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-19 16:50:32 -0400

    Added Selected Features constant

--- lending_club/config.py
+++ lending_club/config.py
@@ -9,3 +9,7 @@ APPROVED_LOANS_CSV = DATAPATH / 'accepted_2007_to_2018Q4.csv'
 
 if not os.path.exists(APPROVED_LOANS_CSV):
     raise OSError(f'{APPROVED_LOANS_CSV} does not exist! Please place in the data directory.')
+
+SELECTED_FEATURES = [  
+
+                    ]
