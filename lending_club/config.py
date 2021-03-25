239d3e029334fb5d7fc0126c89c53930e67b9d6b
David Wasserman <davidphilipwasserman@gmail.com>
2021-03-25 11:36:43 -0400

    Configured raw columns

--- lending_club/config.py
+++ lending_club/config.py
@@ -10,6 +10,34 @@ APPROVED_LOANS_CSV = DATAPATH / 'accepted_2007_to_2018Q4.csv'
 if not os.path.exists(APPROVED_LOANS_CSV):
     raise OSError(f'{APPROVED_LOANS_CSV} does not exist! Please place in the data directory.')
 
+RAW_COLUMNS = [ 'id',
+                'addr_state', # Need to dummify
+                'annual_inc',
+                'application_type', # Need to binarize 
+                'disbursement_method', # Need to binarize
+                'dti',
+                'earliest_cr_line',
+                'emp_length', # Need to convert to number and add NAs
+                'emp_title', # Needs to be encoded
+                'fico_range_high', # Used to derive the average
+                'fico_range_low', # Used to derive the average
+                'grade', # Need to dummify or be ordinal encoded
+                'home_ownership', # Need to dummify
+                'initial_list_status', # Need to dummify (binarize)
+                'installment',
+                'int_rate',
+                'issue_d',
+                'loan_amnt',
+                'open_acc', 
+                'pub_rec', 
+                'pub_rec_bankruptcies',
+                'purpose', # Need to dummify
+                'sub_grade', # Need to dummify or be ordinal encoded
+                'term', # Need to convert to integer from string
+                'verification_status',
+                'zip_code' # Need to dummify
+            ]
+
 SELECTED_FEATURES = ['id',
                      'addr_state', # Need to dummify
                      'annual_inc',
@@ -17,7 +45,7 @@ SELECTED_FEATURES = ['id',
                      'days_since_first_credit', # Derived field
                      'disbursement_method', # Need to binarize
                      'dti',
-                     'earliest_cr_line',
+                     #'earliest_cr_line', Used to derive days_since_first_credit
                      'emp_length', # Need to convert to number and add NAs
                      'emp_title', # Needs to be encoded
                      'fico_score_average', # Derived field
